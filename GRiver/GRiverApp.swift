1bcfe607d10b7363fc314fe1fa8798ea2939883b
Developer <Lepiozdyx@gmail.com>
2025-09-16 15:08:38 -0300

    Initial Commit

---
+++ GRiver/GRiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRiverApp.swift
+//  GRiver
+//
+//  Created by Alex on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GRiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
